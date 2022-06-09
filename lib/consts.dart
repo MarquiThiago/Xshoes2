de9114b1f061251f2a0e98ec79997136e721c1ee
MarquiThiago <85703043+MarquiThiago@users.noreply.github.com>
2022-06-09 19:37:42 -0300

    Update consts.dart

--- lib/consts.dart
+++ lib/consts.dart
@@ -14,4 +14,18 @@ const KVans = AssetImage('images/vans.png');
 
 const TDCshoes =
     Text('descrição: DCshoes tenis feito para seu rolê de skate\n\n'
-        'Valor: RS 1999');
+        'Valor: RS 1999\n\n prazo: 5 dias uteis');
+
+const TVans = Text('descrição: Vans tenis feito para seu rolê de skate\n\n'
+    'Valor: RS 400 \n\n prazo: chega amanha');
+
+const TChuteira =
+    Text('descrição: nike chuteira perfeita para seu footebol de compo\n\n'
+        'Valor: RS 250\n\n prazo: 2 dias uteis');
+
+const TAdidas =
+    Text('descrição: um tenis para corrida mas tambem para seu dia a dia\n\n'
+        'Valor: RS 600\n\n prazo: chega amanha');
+
+const TPuma = Text('descrição: tenis casual para o dia a dia\n\n'
+    'Valor: RS 370\n\n prazo: 5 dias uteis');
